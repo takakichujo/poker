287c4603e56960b33b4f24622e61cdf137c1c162
chujotakaki <takakiking@yahoo.co.jp>
2023-12-05 14:24:36 +0900

    スタイルを適用しました

--- poker_project/src/App.tsx
+++ poker_project/src/App.tsx
@@ -1,7 +1,15 @@
 import './App.css';
 import './index.css';
+import { Button } from './components/atoms/button';
+const aaa = () => {
+  console.log('aaa');
+};
 function App() {
-  return <></>;
+  return (
+    <>
+      <Button onClick={aaa}>{'aaaaa'}</Button>
+    </>
+  );
 }
 
 export default App;
--- poker_project/src/atoms/button/index.tsx
+++
@@ -1,9 +0,0 @@
-import { FC } from 'react';
-
-type Props = {
-  onClick: () => void;
-  name: string;
-};
-export const Button: FC<Props> = ({ name, onClick }) => {
-  return <button onClick={onClick}>{name}</button>;
-};
